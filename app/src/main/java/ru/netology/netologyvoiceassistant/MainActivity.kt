561c2f92d4296b641528dc397675ac9271288886
KKretser <kkret76@outlook.com>
2022-06-18 21:40:52 +0300

    Lesson 2 final

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -5,11 +5,37 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Adapter
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
+    lateinit var requestInput:TextInputEditText
+    lateinit var podsAdapter: SimpleAdapter
+    val pods = mutableListOf<HashMap<String, String>> (
+        HashMap<String,String>().apply {
+            put ("Title","Title 1")
+            put ("Content","Content 1")
+        },
+        HashMap<String,String>().apply {
+            put ("Title","Title 2")
+            put ("Content","Content 2")
+        },
+        HashMap<String,String>().apply {
+            put ("Title","Title 3")
+            put ("Content","Content 3")
+        },
+        HashMap<String,String>().apply {
+            put ("Title","Title 4")
+            put ("Content","Content 4")
+        }
+            )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +46,22 @@ class MainActivity : AppCompatActivity() {
     fun initViews() {
         val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        requestInput=findViewById(R.id.text_input_edit)
+
+        val podsList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title","Content"),
+            intArrayOf(R.id.title,R.id.content))
+
+        podsList.adapter = podsAdapter
+
+        val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
+        voiceInputButton.setOnClickListener {
+            Log.d (TAG, "FlActBut")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,11 +73,14 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_stop -> {
                 Log.d(TAG, "Stop pressed")
+                return true
             }
             R.id.action_clear -> {
                 Log.d(TAG, "Clear pressed")
+                return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
